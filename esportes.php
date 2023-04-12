a50290760706d2fde7dcbf7bf53072356f7c4cfe
Larissa Silva 3 <114114544+cattslari@users.noreply.github.com>
2023-04-11 22:05:18 -0300

    Atividade poney

---
+++ esportes.php
@@ -0,0 +1,12 @@
+<?php include('cabecalho.php') ?>
+<h1><em>Flamengo decide demitir Vítor Pereira após três meses e cinco títulos perdidos</em></h1>
+<h3>Neto detona Flamengo após demissão de Vítor Pereira e revela salário do técnico no Corinthians</h3>
+
+<iframe width="560" 
+height="315" 
+src="https://www.youtube.com/embed/6TDL_L0tfL0" title="YouTube video player" 
+frameborder="0" 
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+ picture-in-picture; web-share" allowfullscreen></iframe>
+ 
+<?php include('footer.php') ?>
